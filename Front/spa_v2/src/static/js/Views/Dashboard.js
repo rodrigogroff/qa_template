8e5bb266fc9f2956ae6ed47124191f51e5a0ee4a
rodrigo groff <rodrigo.groff@gmail.com>
2020-11-11 00:52:01 -0300

    fixs

--- Front/spa_v2/src/static/js/Views/Dashboard.js
+++ Front/spa_v2/src/static/js/Views/Dashboard.js
@@ -12,7 +12,6 @@ export default class extends AbstractView {
 
     getHtml() {
         return  `
-                Dashboard x
                 `;
     }
 }
--- Front/spa_v2/src/static/js/Views/Login.js
+++ Front/spa_v2/src/static/js/Views/Login.js
@@ -3,54 +3,46 @@
 import AbstractView from "../Infra/AbstractView.js"
 import FetchCtrl from "../Infra/FetchCtrl.js";
 
-//Components
-
-
 export default class extends AbstractView {
 
     constructor(params) {
         super(params)
 
         $(document).ready(function () {
-            $('#formMail').focus();
+            $('#formMail').focus()
         });
 
         $(document).on('keydown', function (e) {
-            var code = e.keyCode || e.which;
+            var code = e.keyCode
             if (code === 9) {  // tab
-                e.preventDefault();
-                ValidateForm(false, false);
+                e.preventDefault()
+                validateForm(new FetchCtrl(), { focus: false, msg: false, fields: getData() })
             }
-            else if (code === 13) { // enter
-                btnSubmit_Click()  
-            }                          
+            else if (code === 13) // enter
+                btnSubmit_Click(getData())
         });
 
         $(document).bind('click', function (e) {
             switch ($(e.target).attr('id')) {
-                case 'btnSubmit': btnSubmit_Click(); break;
-                case 'formMail':
-                case 'formPass': break;
-
-                case 'seePass': 
-                    if ($('#seePass').css('color') !== 'rgb(102, 127, 151)')
-                    {
-                        $('#seePass').css('color', $('body').css('color'))
-                        $('#formPass').removeAttr('type');
-                        $('#formPass').attr('type', 'password');
-                    }
-                    else
-                    {
-                        $('#seePass').css('color','green')
-                        $('#formPass').removeAttr('type');
-                        $('#formPass').attr('type', 'text');
-                    }
-                    break;
-
-                // close -> error (click no x)
-                default: ValidateForm(true); break;
+                // login
+                case 'btnSubmit': btnSubmit_Click(getData()); break;
+                // do nothing
+                case 'formMail': case 'formPass': break;
+                // check password
+                case 'seePass': btnSeePassword(); break;
+                // close -> error (x)
+                default: validateForm(new FetchCtrl(), { focus: false, msg: false, fields: getData() }); break;
             }
         });
+
+        function btnSeePassword() {
+            if ($('#seePass').css('color') == $('body').css('color'))
+                $('#seePass').css('color', 'red')
+            else
+                $('#seePass').css('color', $('body').css('color'))
+            $('#formPass').removeAttr('type')
+            $('#formPass').attr('type', 'text')
+        }
 
         function getData() {
             return {
@@ -59,43 +51,43 @@ export default class extends AbstractView {
             };
         }
 
-        function ValidateForm(set_focus, show_msg) {
+        // --api -------------------------
+        // FetchCtrl
+        // --_params -------------------------
+        // focus (true,false)
+        // msg (true,false)
+        // fields (serviceData)
+        // -----------------------------------
 
-            var api = new FetchCtrl();
-            var serviceData = getData();
+        function validateForm(api, _params) {
 
-            var id_failBtnMail = '#failBtnMail'
-            var id_failBtnPass = '#failBtnPass'
-
-            if (!api.isFieldContentValid(serviceData.email, 99, 'email')) {
-                if (set_focus == true)
-                    $('#formMail').focus();            
-                else
-                {
-                    api.errorField(id_failBtnMail)
-                    if (show_msg==true)
-                        api.displaySystemPopup('Error', 'Invalid Email');
+            if (!api.isFieldContentValid(_params.fields.email, 99, 'email')) {
+                if (_params.focus == true)
+                    $('#formMail').focus()
+                else {
+                    api.errorField('#failBtnMail')
+                    if (_params.msg == true)
+                        api.displaySystemPopup('Error', 'Invalid Email')
                 }
                 return false;
             }
             else
-                api.errorClean(id_failBtnMail)
+                api.errorClean('#failBtnMail')
 
             $('#formPass').focus();
 
-            if (!api.isFieldContentValid(serviceData.password, 20, 'password', 4)) {
-                if (set_focus==true)
-                    $('#formPass').focus();
-                else
-                {
-                    api.errorField(id_failBtnPass)
-                    if (show_msg==true)
-                        api.displaySystemPopup('Error', 'Invalid Password');
+            if (!api.isFieldContentValid(_params.fields.password, 20, 'password', 4)) {
+                if (_params.focus == true)
+                    $('#formPass').focus()
+                else {
+                    api.errorField('#failBtnPass')
+                    if (_params.msg == true)
+                        api.displaySystemPopup('Error', 'Invalid Password')
                 }
                 return false;
             }
             else
-                api.errorClean(id_failBtnPass)
+                api.errorClean('#failBtnPass')
 
             // all ok, loose focus
             document.activeElement.blur();
@@ -103,54 +95,54 @@ export default class extends AbstractView {
             return true;
         }
 
-        function btnSubmit_Click() {
+        // --_fields -------------------------
+        // serviceData
 
-            if (!ValidateForm(false, true))
+        function btnSubmit_Click(_fields) {
+
+            var api = new FetchCtrl()
+
+            if (!validateForm(api, { focus: false, msg: true, fields: _fields }))
                 return;
 
-            var api = new FetchCtrl();
-            var serviceData = getData();
+            api.loadingOn()
 
-            api.loadingOn();
-
-            $('#btnSubmit').removeClass('green');
-            $('#btnSubmit').addClass('light');
+            $('#btnSubmit').removeClass('green')
+            $('#btnSubmit').addClass('light')
 
             setTimeout(() => {
-
-                api.postPublicPortal(JSON.stringify(serviceData), 'authenticate_v1')
-                .then(resp => {
-                    api.loadingOff();
-                    $('#btnSubmit').addClass('green');
-                    if (resp.ok === true) {
-                        api.loginOk(
-                            resp.payload.token,
-                            resp.payload.user.email,
-                            resp.payload.user.login,
-                        );
-                        location.href = '/';
-                    } else {
-                        api.cleanLogin();
-                        api.displaySystemPopup('Error', resp.msg);                        
-                    }
-                })
-                .catch(resp => {
-                    api.loadingOff();
-                    $('#btnSubmit').addClass('green');
-                    api.displaySystemPopup('Error', resp.msg);                    
-                });                
-
-            }, 1500);            
+                api.postPublicPortal(JSON.stringify(_fields), 'authenticate_v1')
+                    .then(resp => {
+                        api.loadingOff()
+                        $('#btnSubmit').addClass('green');
+                        if (resp.ok === true) {
+                            api.loginOk(
+                                resp.payload.token,
+                                resp.payload.user.email,
+                                resp.payload.user.login,
+                            );
+                            location.href = '/';
+                        } else {
+                            api.cleanLogin()
+                            api.displaySystemPopup('Error', resp.msg)
+                        }
+                    })
+                    .catch(resp => {
+                        api.loadingOff()
+                        $('#btnSubmit').addClass('green')
+                        api.displaySystemPopup('Error', resp.msg)
+                    });
+            }, 900);
         }
     }
 
     getHtml() {
         return `
             <br>
-            <div style="width:280px" class="form-row-group-dark"> 
+            <div style="width:296px" class="form-row-group-dark"> 
                 <div class="form-divider"></div>
                 <div align='center'>
-                    <h2>Template Login</h2>
+                    <h2>Login</h2>
                 </div>
                 <br>
                 <div class="form-row-group with-icons" align="left">                    
@@ -159,11 +151,11 @@ export default class extends AbstractView {
                         <input id="formMail" type="text" class="form-element" placeholder="Username or Email">
                     </div>
                     <div class="form-row no-padding">
-                        <i class="fa fa-lock" id='failBtnPass'></i>     
+                        <i class="fa fa-lock" style='padding-left:4px' id='failBtnPass'></i>
                         <table width='100%'>
                             <tr>
                                 <td valign='top' width='90%'>
-                                    <input id="formPass" type="password" class="form-element" placeholder="Password">                        
+                                    <input id="formPass" type="password" class="form-element" placeholder="Password">
                                 </td>
                                 <td valign='top'>
                                     <i class="fa fa-eye" id='seePass' title='See password'></i>
@@ -174,19 +166,17 @@ export default class extends AbstractView {
                 </div>
                 <br>                
                 <div class="form-row txt-center">
-                    <a href="forgot-password.html" data-loader="show">Forgot password?</a>
+                    <a href="/forgot">Forgot password?</a>
                 </div>
                 <br>
-                <div class="form-divider"></div>
                 <div class="form-row">
                     <a id="btnSubmit" class="button circle block green">
-                        Login &nbsp;&nbsp;&nbsp;
-                        <i class="fa fa-spinner fa-spin" id='loading' style="font-size:24px;display:none;"></i>
+                        Login <i class="fa fa-spinner fa-spin" id='loading' style="display:none;"></i>
                     </a>
                 </div>
                 <br>
                 <div class="form-row txt-center">
-                    Not registered? <a href="signup.html" data-loader="show">Sign Up</a>
+                    Not registered? <a href="/signup">Sign Up</a>
                 </div>
                 <br>
                 <br>
