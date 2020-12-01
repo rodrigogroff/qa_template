dc6527444fd71617d3bee42630dbdf232d84683f
rodrigo groff <rodrigo.groff@gmail.com>
2020-12-01 04:54:08 -0300

    fixes

--- Front/spa_v1/run.bat
+++
@@ -1,6 +0,0 @@
- xcopy .\src\static\css .\dist\src\static\css /E /D
- xcopy .\src\static\images .\dist\src\static\images /E /D
- xcopy .\src\static\less .\dist\src\static\less /E /D
- xcopy .\src\static\vendor .\dist\src\static\vendor /E /D
-
-node dist/server.js
--- Front/spa_v3/src/static/js/Components/Label.js
+++
@@ -1,6 +0,0 @@
-
-export default class {
-    static getHtml(el, id, text) {
-        return `<${el} id='${el}'>${text}</${el}>`;
-    }
-}
