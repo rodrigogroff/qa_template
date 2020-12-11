667eb799f1384d758b23af8ae9c68f4cb6195420
rodrigo groff <rodrigo.groff@gmail.com>
2020-12-11 09:25:05 -0300

    ajustes

--- Front/nano_v1/src/static/js/Pages/Dashboard/page.js
+++ Front/nano_v1/src/static/js/Pages/Dashboard/page.js
@@ -13,11 +13,6 @@ export default class {
     // ----------------------------------------------------------------
 
     $(document).ready(function () {
-      $('#mainForm').hide();
-      ImgLoader.loadAsync('stats1', 'src/static/img/stats1.png')
-      ImgLoader.loadAsync('stats2', 'src/static/img/stats2.png')
-      ImgLoader.loadAsync('stats3', 'src/static/img/stats3.png')
-
       if (getFromSession("splashed") !== "true") {
         setToSession("splashed", "true");
         $("body").css("background-color", "#FFFFFF");
@@ -37,45 +32,10 @@ export default class {
           }, 2000);
         }, 900);
       }
-      else
-        $('#mainForm').show();
     });
   }
 
   getHtml() {
-
-    var l0 = MultiLanguage(0);
-    var l1 = MultiLanguage(1);
-    var l2 = MultiLanguage(2);
-    var l3 = MultiLanguage(3);
-    var l4 = MultiLanguage(4);
-    var l5 = MultiLanguage(5);
-    var l6 = MultiLanguage(6);
-    var l7 = MultiLanguage(7);
-    var l8 = MultiLanguage(8);
-
-    return `<div id='mainForm'>
-            <div style="width:296px" class="form-row-group-dark"><br>
-            <p>${l0}</p>
-              ${ImgLoader.getHtml('stats1', 'max-width:100%;max-height:100%;padding-top:10px')}
-              <p>${l1}</p>
-              ${ImgLoader.getHtml('stats2', 'max-width:100%;max-height:100%;padding-top:10px')}
-              ${ImgLoader.getHtml('stats3', 'max-width:100%;max-height:100%;padding-top:10px')}
-              <p>${l2}</p>
-              <br>
-              <div align='left'>
-              <b>${l3}</b>
-              <p style='padding-left:10px;font-size:11px'>- ${l4}</p>
-              <p style='padding-left:10px;font-size:11px'>- ${l5}</p>
-              <p style='padding-left:10px;font-size:11px'>- ${l6}</p>
-              <p style='padding-left:10px;font-size:11px'>- ${l7}</p>
-              </div>
-              <br>
-              <button class="button circle block green">${l8}</button>
-              <br>              
-            </div>
-            <br>
-            <br>
-            </div>`;
+    return "";
   }
 }
