57efce79543de691b2beb6ebcaa52a3fabc13eab
rodrigo groff <rodrigo.groff@gmail.com>
2020-12-11 00:00:27 -0300

    ajustes

--- Front/nano_v1/src/static/js/Pages/Dashboard/splash.js
+++ Front/nano_v1/src/static/js/Pages/Dashboard/splash.js
@@ -4,10 +4,11 @@ export default class {
   static getHtml() {
     return `<div id="formSplash" style='width:100%;height:100%;'>
             ${ImgLoader.getDirectHtml(
-              "logoSplash",
-              "src/static/img/splash.png",
-              "max-width:100%;max-height:100%;padding-top:90px"
-            )}
+      "logoSplash",
+      "src/static/img/splash.png",
+      "max-width:100%;max-height:100%;padding-top:90px"
+    )}
+            v1.02
         </div>`;
   }
 }
